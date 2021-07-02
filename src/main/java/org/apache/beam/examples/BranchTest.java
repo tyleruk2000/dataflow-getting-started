ae3fd3ab9ea1c1eb3611dbd9e5e790266deee917
Tyler Allen <tyler.allen@gft.com>
2021-07-02 11:58:48 +0100

    Adding Branch Test

--- src/main/java/org/apache/beam/examples/BranchTest.java
+++ src/main/java/org/apache/beam/examples/BranchTest.java
@@ -15,6 +15,44 @@ import org.apache.beam.sdk.transforms.ToString;
 import org.apache.beam.sdk.values.PCollection;
 
 public class BranchTest {
+
+    static class CreateSystem extends DoFn<Long, Long> {
+        @ProcessElement
+        public void processElement(ProcessContext c){
+            c.output(new Random().nextLong());
+        }
+    }
+
+    static class CreateStar extends DoFn<Long, Long> {
+        @ProcessElement
+        public void processElement(ProcessContext c){
+            int totalStar = new SplittableRandom().nextInt(1, 3);
+            for (int i = 0; i < totalStar; i++) {
+                c.output(new Random().nextLong());
+            }
+        }
+    }
+
+    static class CreatePlanet extends DoFn<Long, Long> {
+        @ProcessElement
+        public void processElement(ProcessContext c){
+            int totalStar = new SplittableRandom().nextInt(3, 15);
+            for (int i = 0; i < totalStar; i++) {
+                c.output(new Random().nextLong());
+            }
+        }
+    }
+
+    static class CreateAsteroid extends DoFn<Long, Long> {
+        @ProcessElement
+        public void processElement(ProcessContext c){
+            int totalStar = new SplittableRandom().nextInt(100, 1000000);
+            for (int i = 0; i < totalStar; i++) {
+                c.output(new Random().nextLong());
+            }
+        }
+    }
+
     public interface BranchTestOptions extends PipelineOptions {
       
     }
@@ -22,11 +60,26 @@ public class BranchTest {
     public static void main(String[] args) {
         BranchTestOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(BranchTestOptions.class);
 
-        ArrayList<Long> seeds = new ArrayList<Long>();
-        seeds.add(0l);
+        ArrayList<Long> seedList = new ArrayList<Long>();
+        seedList.add(0l);
 
         Pipeline p = Pipeline.create(options);
-        PCollection<Long> a =  p.apply(Create.of(seeds));
+        PCollection<Long> seeds =  p.apply(Create.of(seedList));
+        
+        PCollection<Long> systemIds =  seeds.apply("Create System", ParDo.of(new CreateSystem()));
+
+        PCollection<Long> startIds =  systemIds.apply("Create Star", ParDo.of(new CreateStar()));
+
+        PCollection<Long> planetId =  startIds.apply("Create Planet", ParDo.of(new CreatePlanet()));
+        PCollection<Long> moonId =  planetId.apply("Create Moon", ParDo.of(new CreatePlanet()));
+
+        PCollection<Long> asteroidId =  startIds.apply("Create Asteroid", ParDo.of(new CreateAsteroid()));
+
+        systemIds.apply(ToString.elements()).apply("WriteSystem", TextIO.write().to("System"));
+        startIds.apply(ToString.elements()).apply("WriteStar", TextIO.write().to("Star"));
+        planetId.apply(ToString.elements()).apply("WritePlanet", TextIO.write().to("Planet"));
+        moonId.apply(ToString.elements()).apply("WriteMoon", TextIO.write().to("Moon"));
+        asteroidId.apply(ToString.elements()).apply("WriteAsteroid", TextIO.write().to("Asteroid"));
 
         p.run().waitUntilFinish();
     }
